2f2b3d2f09df38371ef3d7d8073c0582f50fbbe8
unknown <vytungocyen@gmail.com>
2020-11-18 09:53:13 +0700

    Tieu Ly Vy Vy

---
+++ demo/Controllers/Check.cs
@@ -0,0 +1,20 @@
+﻿using demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo.Controllers
+{
+    public class Check
+    {
+        public static bool CheckUser(User user)
+        {
+            if (user.Address != null && user.Birthday != null && user.Name != null && user.Password != null
+                && user.Phone != null && user.Username != null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- demo/Controllers/ShopController.cs
+++ demo/Controllers/ShopController.cs
@@ -6,6 +6,12 @@ using System.Web.Mvc;
 using demo.Model;
 using demo.Controllers;
 using System.Web.UI;
+using MailKit.Net.Smtp;
+//using System.Net.Mail;
+using MimeKit;
+using MailKit;
+using MimeKit.Text;
+using MailKit.Security;
 
 namespace demo.Controllers
 {
@@ -37,53 +43,129 @@ namespace demo.Controllers
         {
             return View();
         }
+        public ActionResult ResetPassword()
+        {
+
+            return View();
+        }
+        public ActionResult ForgotPassword()
+        {
+
+            return View();
+        }
+        public ActionResult ForgotPasswordconfirmation()
+        {
+
+            return View();
+        }
         [HttpPost]
         public ActionResult Login(User user)
 
         {
             //Kiểm tra User có tồn tại trong database hay không
             if (user.Username == null || _db.Users.Find(user.Username.Trim()) == null)
-                return View();
+                return Content("false");
             else
             {
                 //Nếu có thì tiến hành kiểm tra ở Form và ở database có trùng khớp hay không
                 var u = _db.Users.Find(user.Username.Trim());
                 if (user.Username.Trim() == u.Username.Trim() && user.Password.Trim() == u.Password.Trim())
                 {
-                    ViewBag.Message = "Thành Công";
                     @ViewBag.user = u.Name.Trim();
                     @ViewBag.isSuccess = "1";
-                    //if(u.isAdmin==true)
-                    //{
-                    //    ViewBag.isAdmin = "1";
-                    //}    
+                    if(u.isAdmin==true)
+                    {
+                        @ViewBag.isAdmin = "1";
+                    }    
                     return View("Home");
                 }
-                else
-                    ViewBag.Message = "Thất Bại";
             }
 
-            return View();
+            return Content("false");
         }
         [HttpPost]
         public ActionResult Register(User user)
         {
-
-            return View();
+            var u = _db.Users.Find(user.Username);
+            if (u != null)
+                return Content("userexist");
+            if (Check.CheckUser(user)==true && u==null)
+            {     
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return View("Login");
+            }
+            
+            else
+            {
+                return Content("false");
+            }
         }
         public ActionResult Home()
 
         {
             return View();
         }
-        public ActionResult Validate(string username, string password)
-
-        {
-            if(String.IsNullOrEmpty(username))
+        [HttpPost]
+        public ActionResult ResetPassword(User user)
+        {//Kiểm tra User có tồn tại trong database hay không
+            if (user.Username == null || _db.Users.Find(user.Username.Trim()) == null)
+                return Content("false");
+            else
             {
-                ModelState.AddModelError("User name", "Không được để trống");
+                //Nếu có thì tiến hành kiểm tra ở Form và ở database có trùng khớp hay không
+                var u = _db.Users.Find(user.Username.Trim());
+                if (user.Username.Trim() == u.Username.Trim() && user.Password.Trim() == u.Password.Trim())
+                {
+                    @ViewBag.user = u.Name.Trim();
+                    @ViewBag.isSuccess = "1";
+                    if (u.isAdmin == true)
+                    {
+                        @ViewBag.isAdmin = "1";
+                    }
+                    return View("Home");
+                }
             }
-            return View("Login");
+            return View("");
         }
+        [HttpPost]
+        public ActionResult ForgotPassword(User user)
+        {
+            if (user.Email != null)
+            {
+                var x = _db.Users.Where(u => u.Email.Contains(u.Email));
+                var MailTo = user.Email.Trim();
+                //var u = _db.Users.Find(f["email"]);
+                if (x == null)
+                    return Content("false");
+                else
+                {
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress("Thien-Vy's Watch Store", "watchstorethienvys@gmail.com"));
+                    message.To.Add(new MailboxAddress("Custom", MailTo));
+                    message.Subject = "Password recovery";
+                    message.Body = new TextPart("plain")
+                    {
+                        Text = "Link: https://localhost:44305/Shop/ResetPassword"
+                    };
+                    using (var client = new SmtpClient())
+                    {
+                        client.Connect("smtp.gmail.com", 587, false);
+                        client.Authenticate("watchstorethienvys@gmail.com", "thienvy123");
+                        client.Send(message);
+                        client.Disconnect(true);
+                    }
+                    return View("ForgotPasswordconfirmation");
+                }
+            }
+            else
+            {
+                ViewBag.textbox = "false";
+                
+                return Content("false");
+            }
+            return View();
+        }
+
     }
 }
